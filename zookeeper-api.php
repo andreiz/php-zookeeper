0a6037ccf6bc3574d5677e74063a3cfebebddd15
Andrei Zmievski <andrei@zmievski.org>
2010-05-21 14:37:13 -0700

    Clean up whitespace and add new API call.

--- zookeeper-api.php
+++ zookeeper-api.php
@@ -9,14 +9,16 @@ class Zookeeper {
     /* if host is provided, attempt to connect */
 	public function __construct( $host = '', $watcher_cb = null, $recv_timeout = 10000) {}
 
+	public function connect( $host, $watcher_cb = null, $recv_timeout = 10000) {}
+
 	public function create( $host, $watcher_cb = null, $recv_timeout = 10000) {}
-	
+
 	public function create( $path, $value, $acl, $flags = null ) {}
 
 	public function delete( $path, $version = -1 ) {}
 
 	public function set( $path, $data, $version = -1, &$stat = null ) {}
-	
+
 	public function get( $path, $watcher_cb = null, &$stat = null) {}
 
 	public function getChildren( $path, $watcher_cb = null ) {}
@@ -38,19 +40,19 @@ class Zookeeper {
 	public function addAuth( $scheme, $cert, $completion_cb = null ) {}
 
 	public function isRecoverable( ) {}
-	
+
 	public function setLogFile( $file ) {} // TODO: might be able to set a stream like php://stderr or something
 
 	public function close( ) {}
 
 	public function getResultMessage( ) {}
-	
-	
+
+
 	// static methods
-	
+
 	static public function setDebugLevel( $level ) {}
 
 	static public function setDeterministicConnOrder( $trueOrFalse ) {}
-	
+
 }
 
